920a2accc6ad52266969fa31903d25f704af011f
Prinu Dickson <prinudickson@gmail.com>
2023-09-13 13:30:02 +0200

    added delete and update employer mutations

--- app/gql/employer/mutations.py
+++ app/gql/employer/mutations.py
@@ -1,4 +1,4 @@
-from graphene import Mutation, String, Field
+from graphene import Mutation, String, Field, Int, Boolean
 from app.gql.types import EmployerObject
 from app.db.database import SessionLocal
 from app.db.models import Employer
@@ -20,3 +20,54 @@ class AddEmployer(Mutation):
         session.refresh(employer)
         #session.close()
         return AddEmployer(employer=employer)
+    
+
+class UpdateEmployer(Mutation):
+    class Arguments:
+        id = Int(required=True)
+        name = String()
+        contact_email = String()
+        industry = String()
+
+    employer = Field(lambda: EmployerObject)
+
+    @staticmethod
+    def mutate(root, info, id, name=None, contact_email=None, industry=None):
+        session = SessionLocal()
+        em = session.query(Employer).filter(Employer.id==id).first()
+
+        if not em:
+            raise Exception("Job not found")
+        
+        if name is not None:
+            em.name = name
+        
+        if contact_email is not None:
+            em.contact_email = contact_email
+        
+        if industry is not None:
+            em.industry = industry
+
+        session.commit()
+        session.refresh(em)
+        #session.close()
+        return UpdateEmployer(employer=em)
+    
+class DeleteEmployer(Mutation):
+    class Arguments:
+        id = Int(required=True)
+
+    success = Boolean()
+
+    @staticmethod
+    def mutate(root, info, id): #title=None, description=None, employer_id=None):
+        session = SessionLocal()
+        em = session.query(Employer).filter(Employer.id==id).first()
+
+        if not em:
+            raise Exception("Job not found")
+
+        session.delete(em)
+        session.commit()
+        session.close()
+        return DeleteEmployer(success=True)
--- app/gql/mutations.py
+++ app/gql/mutations.py
@@ -1,10 +1,12 @@
 from graphene import ObjectType
 from app.gql.job.mutations import AddJob, UpdateJob, DeleteJob
-from app.gql.employer.mutations import AddEmployer
+from app.gql.employer.mutations import AddEmployer, UpdateEmployer, DeleteEmployer
     
 class Mutation(ObjectType):
     add_job  = AddJob.Field()
     update_job = UpdateJob.Field()
     delete_job = DeleteJob.Field()
     add_employer = AddEmployer.Field()
+    update_employer = UpdateEmployer.Field()
+    delete_employer = DeleteEmployer.Field()
 
